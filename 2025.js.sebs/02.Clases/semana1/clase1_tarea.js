b9c4291aabe078e631e694b7bf7cdfebcbb8522a
filthycbaz <cbaz2080@gmail.com>
2025-08-13 00:33:51 -0600

    ✅ hice mi calculadora en js

--- 2025.js.sebs/02.Clases/semana1/clase1_tarea.js
+++ 2025.js.sebs/02.Clases/semana1/clase1_tarea.js
@@ -2,10 +2,28 @@ const { ask } = require('../helpers/input.js');
 
 async function main() {
   // TODO: Crear una calculadora simple que realice operaciones básicas:
-  // 1.- Pedir el primer número
-  // 2.- Pedir el segundo número
-  // 3.- Pedir operador
+let no1 = Number(await ask ("echame el primer numero")); // 1.- Pedir el primer número
+let no2 = Number(await ask("echame el segundo numero"));  // 2.- Pedir el segundo número
+let operador = await ask ("dame el operador (+, -, *, /):"); // 3.- Pedir operador
+let resultado
+if (operador === "+" ) {
+  resultado = no1 + no2;
+} else if (operador === "-"){
+  resultado = no1 - no2;
+} else if (operador === "*"){ 
+  resultado = no1 * no2;
+} else if (operador === "/"){
+  if (no2 === 0) {
+    console.log("nel carnal no se puede eso, entre cero no");
+    return;
+  }
+  resultado = no1 / no2
+} else {
+  console.log("este no existe loco");
+  return;
+}
 
+  console.log(`mira aqui esta el resultado: ${resultado}`)
 }
 
 main();
